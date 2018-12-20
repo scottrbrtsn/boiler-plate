86239aaec67fa73817183f1436ea0c6e1623b960
Scott Robertson <scott.robertson@saic.com>
2018-12-20 11:35:42 -0600

    more generic boiler

--- src/main/java/com/scottrbrtsn/spring/boiler/plate/services/impl/LogService.java
+++ src/main/java/com/scottrbrtsn/spring/boiler/plate/services/impl/LogService.java
@@ -1,12 +1,15 @@
 package com.scottrbrtsn.spring.boiler.plate.services.impl;
 
+import com.scottrbrtsn.spring.boiler.plate.ras.ILogsRepository;
 import com.scottrbrtsn.spring.boiler.plate.services.ILogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LogService implements ILogService {
 
-
+    @Autowired
+    ILogsRepository logsRepository;
 
 }
 
